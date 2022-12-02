03e08d6646291064adfcf7313304a95f8f3c0ab3
sk8erchoi <sk8er.choi@gmail.com>
2022-12-02 18:49:28 +0900

    feat: support pdf file

--- exfigs.py
+++ exfigs.py
@@ -4,6 +4,8 @@ import os
 import pathlib
 import sys
 import zipfile
+import fitz  # pip install --upgrade pip; pip install --upgrade pymupdf
+from tqdm import tqdm # pip install tqdm
 
 
 def extract_figs(path):
@@ -17,9 +19,25 @@ def extract_figs(path):
         z.close()
 
 
+def extract_figs_from_PDF(path):
+    if os.path.isfile(path) and path.endswith('.pdf'):
+        doc = fitz.Document(path)
+        for i in tqdm(range(len(doc)), desc="pages"):
+            for img in tqdm(doc.get_page_images(i), desc="page_images"):
+                xref = img[0]
+                image = doc.extract_image(xref)
+                pix = fitz.Pixmap(doc, xref)
+                pix.save("%s_p%s-%s.png" % (path[:-4], i, xref))
+    print("Done!")
+
+
 if __name__ == '__main__':
     arg1 = sys.argv[1]
-    if not os.path.isfile(arg1) or not arg1.endswith('.docx'):
+    if not os.path.isfile(arg1):
         sys.exit(1)
-    else:
+    elif arg1.endswith('.docx'):
         extract_figs(arg1)
+    elif arg1.endswith('.pdf'):
+        extract_figs_from_PDF(arg1)
+    else:
+        sys.exit(1)
