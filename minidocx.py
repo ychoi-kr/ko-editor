47906aa71261e3e24db4e33cb0af602e79c7a950
Yong Choi <ychoi_kr@naver.com>
2023-02-15 23:18:06 +0900

    feat: extract files into temp dir

--- minidocx.py
+++ minidocx.py
@@ -15,32 +15,37 @@ def minimize(docx):
     return _resize_images_in_zipfile(docx, scale)
 
 
-def _resize_images_in_zipfile(zipfile_path, scale):
-    if not (os.path.isfile(zipfile_path) and Path(zipfile_path).suffix == '.docx'):
-        print("Not a .docx file: " + zipfile_path)
+def _resize_images_in_zipfile(zip_path, scale):
+    in_zip_path = os.path.abspath(zip_path)
+
+    if not (os.path.isfile(in_zip_path) and Path(in_zip_path).suffix == '.docx'):
+        print("Not a .docx file: " + in_zip_path)
         sys.exit(1)
+    input_zip = zipfile.ZipFile(in_zip_path, 'r')
+    out_zip_path = os.path.splitext(in_zip_path)[0] + '_mini.docx'
 
-    input_zip = zipfile.ZipFile(zipfile_path, 'r')
-    output_zip = zipfile.ZipFile(
-        os.path.splitext(zipfile_path)[0] + '_mini.docx',
-        'w'
-    )
-
-    for item in input_zip.infolist():
-        filename = item.filename
-        if filename.startswith('word/media/') and filename.split('.')[-1] in ['jpg', 'png']:
-            input_zip.extract(item)
-            img = Image.open(filename)
-            resized_img = img.resize(
-                (int(img.width * scale), int(img.height * scale))
-            )
-            resized_img.save(filename)
-            output_zip.write(filename)
-        else:
-            output_zip.writestr(item, input_zip.read(filename))
+    working_dir = os.getcwd()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        os.chdir(tmpdir)
+        tmp_zip_path = os.path.join(tmpdir, os.path.basename(in_zip_path))
+        tmp_zip = zipfile.ZipFile(tmp_zip_path, 'w')
     
-    input_zip.close()
-    output_zip.close()
+        for item in input_zip.infolist():
+            filename = item.filename
+            if filename.startswith('word/media/') and filename.split('.')[-1] in ['jpg', 'png']:
+                input_zip.extract(item)
+                img = Image.open(filename)
+                resized_img = img.resize(
+                    (int(img.width * scale), int(img.height * scale))
+                )
+                resized_img.save(filename)
+                tmp_zip.write(filename)
+            else:
+                tmp_zip.writestr(item, input_zip.read(filename))
+        
+        input_zip.close()
+        tmp_zip.close()
+        shutil.copy(tmp_zip_path, out_zip_path)
 
 
 if __name__ == '__main__':
