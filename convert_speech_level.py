47fb465b6fe629f2a0eab1e3bf57d917bd598147
Yong Choi <ychoi_kr@naver.com>
2023-05-25 16:35:52 +0900

    feat: add argument for source level

--- convert_speech_level.py
+++ convert_speech_level.py
@@ -1,22 +1,24 @@
 import os
+import sys
 
-def load(fp, level):
+def load(fp, level, src_che=None):
+    src_idx = ['hasipsio', 'haeyo', 'haera', 'hae'].index(src_che) if src_che else None
 
     tt = list()
 
     for line in fp.readlines()[1:]:
         col = line.rstrip().split(',')  # hasipsio, haeyo, haera, hae
         if level == 'hasipsio':
-            src_idx_list = [1, 2, 3]
+            src_idx_list = [src_idx] if src_che else [1, 2, 3]
             dst_idx = 0
         elif level == 'haeyo':
-            src_idx_list = [0, 2, 3]
+            src_idx_list = [src_idx] if src_che else [0, 2, 3]
             dst_idx = 1
         elif level == 'haera':
-            src_idx_list = [0, 1, 3]
+            src_idx_list = [src_idx] if src_che else [0, 1, 3]
             dst_idx = 2
         elif level == 'hae':
-            src_idx_list = [0, 1, 2]
+            src_idx_list = [src_idx] if src_che else [0, 1, 2]
             dst_idx = 3
         
         for src_idx in src_idx_list:
@@ -27,7 +29,8 @@ def load(fp, level):
                 ))
     return tt
 
-def convert(sentence, level):
+def convert(sentence, level, src_che=None):
+
     fp = open(
             os.path.join(os.path.dirname(os.path.realpath(__file__)),
             'ko_speech_level.txt'
@@ -36,27 +39,8 @@ def convert(sentence, level):
         encoding="UTF8"
     )
     
-    tt = load(fp, level)
+    tt = load(fp, level, src_che)
 
-    for line in fp.readlines()[1:]:
-        col = line.rstrip().split(',')  # hasipsio, haeyo, haera, hae
-        if level == 'hasipsio':
-            src_idx_list = [1, 2, 3]
-            dst_idx = 0
-        elif level == 'haeyo':
-            src_idx_list = [0, 2, 3]
-            dst_idx = 1
-        elif level == 'haera':
-            src_idx_list = [0, 1, 3]
-            dst_idx = 2
-        elif level == 'hae':
-            src_idx_list = [0, 1, 2]
-            dst_idx = 3
-        
-        for src_idx in src_idx_list:
-            if col[src_idx] != '' and col[dst_idx] != '':
-                tt.append((col[src_idx], col[dst_idx]))
-   
     for src, dst in tt:
         for sb in " ,.!?":
             sentence = sentence.replace(src + sb, dst + sb)
@@ -64,15 +48,15 @@ def convert(sentence, level):
 
     return sentence
 
-def hasipsio(sentence):
-    return convert(sentence, 'hasipsio')
+def hasipsio(sentence, src_che=None):
+    return convert(sentence, 'hasipsio', src_che)
 
-def haeyo(sentence):
-    return convert(sentence, 'haeyo')
+def haeyo(sentence, src_che=None):
+    return convert(sentence, 'haeyo', src_che)
 
-def haera(sentence):
-    return convert(sentence, 'haera')
+def haera(sentence, src_che=None):
+    return convert(sentence, 'haera', src_che)
 
-def hae(sentence):
-    return convert(sentence, 'hae')
+def hae(sentence, src_che=None):
+    return convert(sentence, 'hae', src_che)
 
--- hasipsio.py
+++ hasipsio.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
+
+import sys
 import convert_speech_level
 
+src_che = sys.argv[1] if len(sys.argv) == 2 else None
 sentence = input()
 print()
 print(convert_speech_level.hasipsio(sentence))
