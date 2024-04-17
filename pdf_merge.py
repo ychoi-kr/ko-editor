eb9d30353e427acd7cdba6c9f5a7d188bd26652e
sk8erchoi <sk8er.choi@gmail.com>
2024-04-17 18:35:22 +0900

    Update PDF merging for PyPDF2 v3 compatibility

--- pdf_merge.py
+++ pdf_merge.py
@@ -4,17 +4,26 @@ import os
 
 from natsort import natsorted
 
+import PyPDF2
 
-try:
-    from PyPDF2 import PdfMerger as Merger
-except ImportError:
-    from PyPDF2 import PdfFileMerger as Merger
-finally:
-    from PyPDF2 import PdfFileReader
+if PyPDF2.__version__ < '3.0.0':
+    from PyPDF2 import PdfFileMerger as PdfMerger
+    PdfMerger.add_outline_item = PdfMerger.addBookmark
+    from PyPDF2 import PdfFileReader as PdfReader
+else:
+    from PyPDF2 import PdfMerger
+    from PyPDF2 import PdfReader
+
+
+def getnumpages(reader):
+    if PyPDF2.__version__ < '3.0.0':
+        return reader.getNumPages
+    else:
+        return len(reader.pages)
 
 
 def main(book_title, directory, sub_dir='merged'):
-    merger = Merger()
+    merger = PdfMerger()
     current_page = 0
     
     pattern = re.compile(rf'{re.escape(book_title)}.*\.pdf')
@@ -22,11 +31,12 @@ def main(book_title, directory, sub_dir='merged'):
     sorted_file_list = natsorted(file_list)
 
     for f in sorted_file_list:
-        reader = PdfFileReader(f)
+        reader = PdfReader(f)
         bookmark_title = os.path.splitext(os.path.basename(f))[0]
         merger.append(f)
-        merger.addBookmark(bookmark_title, pagenum=current_page)
-        current_page += reader.getNumPages()
+        merger.add_outline_item(bookmark_title, pagenum=current_page)
+
+        current_page += getnumpages(reader)
 
     os.chdir(directory)
     if not os.path.isdir(sub_dir):
